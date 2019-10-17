adc368a0817aaa78835ebfc6b883b92423a028b3
William Patrick Mccormack <william.patrick.mc.cormack.iii@cern.ch>
2019-10-17 16:29:32 -0700

    stashing some changes that Robert made

--- analysis/source/LowPtAnalysis/python/runTruthAnalysis_eljob.py
+++ analysis/source/LowPtAnalysis/python/runTruthAnalysis_eljob.py
@@ -52,7 +52,7 @@ elif (options.debug == 2):
 
 # Tweak settings, if necessary
 #alg.setProperty("input_trk_eff_file", "/global/homes/s/spgriso/code/AtlasExclWW/tracking/trk-exclusive-ww/analysis/data/trk_eff_pt.root")
-alg.input_trk_eff_file = "/global/homes/s/spgriso/code/AtlasExclWW/tracking/trk-exclusive-ww/analysis/data/trk_eff_pt.root"
+alg.input_trk_eff_file = "/global/homes/s/spgriso/code/AtlasExclWW/trk-exclusive-ww/analysis/data/trk_eff_pt.root" #Newer trk_eff
 alg.filter_by_selections=True
 alg.tracks_min_pt=100.0 # low-pT tracking
 
