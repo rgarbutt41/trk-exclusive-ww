fe44dbd5f47943080b87ea874d66e41cb5f9afc0
rgarbutt41 <rgarbutt@berkeley.edu>
2020-04-17 11:22:45 -0700

    Includes Errors.txt in script, currently set to opp flavor

--- analysis/Robert_scripts/Bash_scripts/Apply_Selections.sh
+++ analysis/Robert_scripts/Bash_scripts/Apply_Selections.sh
@@ -9,18 +9,20 @@ do
     cd ../
     for num in {100..500..50};
     do
-	for i in InclWW ExclWW; #DY Ztautau Zmumu;
+	for i in ExclWW InclWW;# DY Ztautau Zmumu;
 	do
 	    if [ "$i" = "DY" ];
 	    then
 		cd $i
 		echo $num
 		touch Ratios.txt
+		touch Errors.txt
 		sh /global/cfs/projectdirs/atlas/rgarbutt/trk-exclusive-ww/analysis/Robert_scripts/Bash_scripts/TruthAnalysis.sh $num DYmumu
 		cd ../
 	    else
 		cd $i
 		touch Ratios.txt
+		touch Errors.txt
 	    sh /global/cfs/projectdirs/atlas/rgarbutt/trk-exclusive-ww/analysis/Robert_scripts/Bash_scripts/TruthAnalysis.sh $num $i
 	    cd ../
 	    fi
